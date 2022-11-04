0c9c90f574a9ec5d58d5430d56c824f4ac884ac7
Jackson <jackson.guima2@gmail.com>
2022-11-04 11:39:34 -0300

    Configurando templates

--- src/app.py
+++ src/app.py
@@ -1,11 +1,16 @@
 from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from flask_simplemde import SimpleMDE
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
+mde = SimpleMDE(app)
+
 
 @app.route('/')
 @app.route('/home')
 def home():
-   return render_template("home.html")
+   return render_template("base.html")
 
 if __name__ == '__main__':
     app.run()
