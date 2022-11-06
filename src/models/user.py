7e2762cb9cdf0f418f742e08e8cbb7cea650311b
guilhermealbm <gui.miranda.13@gmail.com>
2022-11-06 13:09:16 -0300

    update mutable PickleType

--- src/models/user.py
+++ src/models/user.py
@@ -1,3 +1,6 @@
+from sqlalchemy import PickleType
+from sqlalchemy.ext.mutable import MutableList
+
 from app import db
 from flask_login import UserMixin
 
@@ -9,8 +12,8 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(255))
     email = db.Column(db.String(255), unique=True, index=True)
     password = db.Column(db.String(255))
-    wantToWatchList = db.Column(db.PickleType, nullable=True)
-    watchedList = db.Column(db.PickleType, nullable=True)
+    wantToWatchList = db.Column(MutableList.as_mutable(PickleType))
+    watchedList = db.Column(MutableList.as_mutable(PickleType))
 
     def verify_password(self, password):
         return self.password == password
