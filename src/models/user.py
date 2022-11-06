d6ac15eabbc9e56e9d70572898e67f3a0f1d81fa
guilhermealbm <gui.miranda.13@gmail.com>
2022-11-06 12:54:31 -0300

    update db

--- src/models/user.py
+++ src/models/user.py
@@ -1,6 +1,7 @@
 from app import db
 from flask_login import UserMixin
 
+
 class User(UserMixin, db.Model):
     __tablename__ = 'users'
 
@@ -8,6 +9,8 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(255))
     email = db.Column(db.String(255), unique=True, index=True)
     password = db.Column(db.String(255))
+    wantToWatchList = db.Column(db.PickleType, nullable=True)
+    watchedList = db.Column(db.PickleType, nullable=True)
 
     def verify_password(self, password):
         return self.password == password
