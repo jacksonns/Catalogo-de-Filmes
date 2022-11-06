59873de31fc2cc95287d1c66b889dad2af96724a
Jackson <jackson.guima2@gmail.com>
2022-11-06 15:39:33 -0300

    Resolvendo conflitos

--- src/models/movie.py
+++ src/models/movie.py
@@ -1,3 +1,5 @@
+import requests
+
 class Movie:
 
     def __init__(self, id, title, synopsis, poster):
@@ -5,3 +7,33 @@ class Movie:
         self.title = title
         self.synopsis = synopsis
         self.poster = 'https://image.tmdb.org/t/p/w500/' + poster
+
+
+def getMovieDetails(id, API_KEY, MOVIE_BASE_URL):
+    get_movie_details_url = MOVIE_BASE_URL.format(id, API_KEY)
+    movie_details_response = requests.get(get_movie_details_url).json()
+    id = movie_details_response.get('id')
+    title = movie_details_response.get('original_title')
+    overview = movie_details_response.get('overview')
+    poster = movie_details_response.get('poster_path')
+
+    return Movie(id, title, overview, poster)
+
+
+def getMoviesList(movies_url):
+    movies_response = requests.get(movies_url).json()
+    movie_results = []
+    if movies_response['results']:
+        movie_results_list = movies_response['results']
+
+        for movie_item in movie_results_list:
+            id = movie_item.get('id')
+            title = movie_item.get('original_title')
+            overview = movie_item.get('overview')
+            poster = movie_item.get('poster_path')
+
+            if poster:
+                movie_object = Movie(id, title, overview, poster)
+                movie_results.append(movie_object)
+
+    return movie_results
